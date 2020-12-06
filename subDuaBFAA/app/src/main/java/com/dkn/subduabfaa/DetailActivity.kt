163e2a8a51dfa4369c882ad0ed3cefd981fe96ea
dwikrisnandi <72020175+dwikrisnandi@users.noreply.github.com>
2020-12-06 19:13:34 +0700

    Add files via upload

---
+++ subDuaBFAA/app/src/main/java/com/dkn/subduabfaa/DetailActivity.kt
@@ -0,0 +1,54 @@
+package com.dkn.subduabfaa
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_row_users.username
+import kotlinx.android.synthetic.main.activity_detail.*
+
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_DATA = "extra_data"
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setData()
+        viewPagerConfig()
+    }
+
+    private fun viewPagerConfig() {
+        val viewPagerDetailAdapter = ViewPagerDetailAdapter(this, supportFragmentManager)
+        view_pager.adapter = viewPagerDetailAdapter
+        tabs.setupWithViewPager(view_pager)
+        supportActionBar?.elevation = 0f
+    }
+
+
+    private fun setActionBarTitle(title: String) {
+        if (supportActionBar != null) {
+            supportActionBar!!.title = title
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setData() {
+        val dataUser = intent.getParcelableExtra<DataUsers>(EXTRA_DATA) as DataUsers
+        setActionBarTitle(dataUser.name.toString())
+        name.text = dataUser.name.toString()
+        username.text = "( " + dataUser.username.toString() + " )"
+        company.text = dataUser.company.toString()
+        location.text = dataUser.location.toString()
+        repo.text = dataUser.repository.toString()
+        followerss.text = dataUser.followers.toString()
+        followings.text = dataUser.following.toString()
+        Glide.with(this)
+            .load(dataUser.avatar.toString())
+            .into(avatars)
+    }
+}
---
+++ subDuaBFAA/app/src/main/java/com/dkn/subduabfaa/ViewPagerDetailAdapter.kt
@@ -0,0 +1,34 @@
+package com.dkn.subduabfaa
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerDetailAdapter(private val mContext: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val pages = listOf(
+        FollowersFragment(),
+        FollowingFragment()
+    )
+
+    @StringRes
+    private val tabTitle = intArrayOf(
+        R.string.followers,
+        R.string.following
+    )
+
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(tabTitle[position])
+    }
+}
